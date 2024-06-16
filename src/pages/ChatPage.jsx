468cd18c92f0ad8f55fe451f7f320d9a2531a740
tomhoon <gnsdl9108@naver.com>
2024-06-16 21:08:13 +0900

    test: 채팅테스트 sockjs로 변경:

--- src/pages/ChatPage.jsx
+++ src/pages/ChatPage.jsx
@@ -1,36 +1,26 @@
 import React, { useEffect, useState } from 'react';
 
-import Stomp from '@stomp/stompjs'
-import { Client } from '@stomp/stompjs'
+import Stomp from '@stomp/stompjs';
+import { Client } from '@stomp/stompjs';
+
+// import SockJS from 'sockjs-client';
 
 const ChatPage = () => {
-  const [message, setMessage] = useState('');
-  const [stompClient, setStompClient] = useState(null)
+
+  const connectHandler = () => {
+   let sockJs = new SockJS("/ws/chat", null, {transports: ["websocket", "xhr-streaming", "xhr-polling"]});
+
+    
+  };
 
   useEffect(() => {
-
-    const stomp = new Client({
-      brokerURL: 'wss://namu0005.or.kr/api/ws-zelkova',
-      connectHeaders: {
-        // Authorization: `Bearer ${access}`,
-      },
-      debug: (str) => {
-        console.log(str)
-      },
-      reconnectDelay: 5000, //자동 재 연결
-      heartbeatIncoming: 4000,
-      heartbeatOutgoing: 4000,
-    })
-    setStompClient(stomp)
-    
-    stomp.activate()
-
+    connectHandler();
   }, []);
 
   return (
     <div>
       <h1>WebSocket Message</h1>
-      <p>{message}</p>
+      {/* <p>{message}</p> */}
     </div>
   );
 };
