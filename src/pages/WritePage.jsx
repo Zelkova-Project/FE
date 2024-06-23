68236dd187ddd6cdc647d9f628723273c2c398a8
tomhoon <gnsdl9108@naver.com>
2024-06-23 16:37:45 +0900

    feat: 에디터 작업중

--- src/pages/WritePage.jsx
+++ src/pages/WritePage.jsx
@@ -1,19 +1,20 @@
-import Section from '../components/Section';
-import Nav from '../components/Nav';
-import Footer from '../components/Footer';
-import '../css/write.css';
 import axios from '../axios/axiosInstance';
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { subtitState, navIdxState } from '../recoilState/recoil';
+import React, { useEffect, useState } from 'react';
+
+import { navIdxState, activeInfoState } from '../recoilState/recoil';
 import { useRecoilState } from 'recoil';
+import { useNavigate } from 'react-router-dom';
+
+import '../css/write.css';
+
+import PageLayout from '../components/common/PageLayout';
+import WriteBody from '../components/write/WriteBody';
 
 const WritePage = () => {
-  window.scrollTo(0, 0);
-
   const navigate = useNavigate();
-  const [subtit, setSubtit] = useRecoilState(subtitState);
   const [navIdx, setNavIdx] = useRecoilState(navIdxState);
+
+  const [activeInfo, setActiveInfo] = useRecoilState(activeInfoState);
 
   const [postInfo, setPostInfo] = useState({
     category: 'BOARD',
@@ -27,6 +28,10 @@ const WritePage = () => {
     home: require('../imgs/notice/가정통신문main.png'),
     hire: require('../imgs/notice/채용안내main.png'),
   };
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
   const getMainImg = () => {
     const imgList = [imgObj.notice, imgObj.home, imgObj.hire];
@@ -65,123 +70,49 @@ const WritePage = () => {
   };
 
   return (
-    <div className="main-container">
-      <Nav />
-      <Section>
-        <div className="guide-img-container">
-          <div className="img-title">
-            <h3>{subtit}</h3>
-          </div>
-          <img className="main-img" src={getMainImg()} alt="main-section"></img>
+    // <div className="main-container">
+    //   <Nav />
+    //   <Section>
+    //     <div className="guide-img-container">
+    //       <div className="img-title">
+    //         {/* <h3>{subtit}</h3> */}
+    //       </div>
+    //       <img className="main-img" src={getMainImg()} alt="main-section"></img>
 
-          <div className="sub-nav-wrapper">
-            <div className="sub-nav">
-              <ul>
-                <li>
-                  <span onClick={() => activeHandler(0)} className={navIdx == 0 ? 'active' : ''}>
-                    공지사항
-                  </span>
-                </li>
-                <li>
-                  <span
-                    onClick={() => activeHandler(1)}
-                    className={navIdx == 1 ? 'active-wide' : ''}
-                  >
-                    가정통신문
-                  </span>
-                </li>
-                <li>
-                  <span onClick={() => activeHandler(2)} className={navIdx == 2 ? 'active' : ''}>
-                    채용안내
-                  </span>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </Section>
-
+    //       <div className="sub-nav-wrapper">
+    //         <div className="sub-nav">
+    //           <ul>
+    //             <li>
+    //               <span onClick={() => activeHandler(0)} className={navIdx == 0 ? 'active' : ''}>
+    //                 공지사항
+    //               </span>
+    //             </li>
+    //             <li>
+    //               <span
+    //                 onClick={() => activeHandler(1)}
+    //                 className={navIdx == 1 ? 'active-wide' : ''}
+    //               >
+    //                 가정통신문
+    //               </span>
+    //             </li>
+    //             <li>
+    //               <span onClick={() => activeHandler(2)} className={navIdx == 2 ? 'active' : ''}>
+    //                 채용안내
+    //               </span>
+    //             </li>
+    //           </ul>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </Section>
+    <>
       {/* 글쓰기영역시작 */}
-      <Section>
-        <div className="write-outer-container">
-          <div className="write-flexitem">
-            <h5 className="write-subtit">{subtit} 등록 및 수정</h5>
-          </div>
-          <div className="write-flexItem">
-            <h3>제목</h3>
-            <input onChange={(e) => setPostInfo({ ...postInfo, title: e.target.value })}></input>
-          </div>
-          <div className="write-flexItem">
-            <div className="write-flexSub">
-              <h3>작성자</h3>
-              <input></input>
-            </div>
-            <div className="write-flexSub ml-40">
-              <h3>작성일</h3>
-              <input readOnly value="2024-05-30"></input>
-            </div>
-          </div>
-
-          <div className="write-flexItem">
-            <div className="write-flexSub">
-              <h3>게시 여부</h3>
-              <select>
-                <option>공개</option>
-                <option>비공개</option>
-              </select>
-            </div>
-            <div className="write-flexSub ml-40">
-              <h3>수정일</h3>
-              <input readOnly value="2024-05-30"></input>
-            </div>
-          </div>
-
-          <div className="write-flexItem mt-50">
-            <div className="write-textArea">
-              <h3>내용작성</h3>
-              <textarea
-                cols="40"
-                rows="40"
-                onChange={(e) => setPostInfo({ ...postInfo, content: e.target.value })}
-              ></textarea>
-            </div>
-          </div>
-
-          <div className="write-flexItem mt-50">
-            <div className="write-flexSub">
-              <h3>첨부파일</h3>
-            </div>
-
-            <div className="write-file-list ml-40">
-              <ul>
-                <li>
-                  <h3>선택된 파일명</h3>
-                </li>
-                <li>
-                  <h3>선택된 파일명</h3>
-                </li>
-              </ul>
-            </div>
-
-            <div className="write-file-btns ml-20">
-              <button>파일 삭제</button>
-              <button>파일 추가</button>
-            </div>
-          </div>
-
-          <div className="write-flexItem dja-center mt-50 mb-50">
-            <div className="write-submit-btns">
-              <button onClick={() => navigate(-1)}>취소</button>
-              <button>삭제</button>
-              <button onClick={() => goWrite()}>등록</button>
-            </div>
-          </div>
-        </div>
-      </Section>
+      <PageLayout pageName={activeInfo.activePage} bodySlot={<WriteBody />} />
       {/* 글쓰기영역끝 */}
+    </>
 
-      <Footer />
-    </div>
+    // <Footer />
+    // </div>
   );
 };
 
