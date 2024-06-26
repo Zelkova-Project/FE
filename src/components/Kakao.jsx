d3d7ef11dadb16c13c2f963fb466dcc0d3661ef7
남병현 <kmi5063@naver.com>
2024-06-26 17:17:47 +0900

    style : 메인화면 가운데 정렬

--- src/components/Kakao.jsx
+++ src/components/Kakao.jsx
@@ -8,7 +8,11 @@ const Kakao = () => {
   });
   return (
     <>
-      <Map center={state.center} style={{ width: '1000px', height: '400px' }} level={3}>
+      <Map
+        center={state.center}
+        style={{ width: '1000px', height: '400px', margin: '0 auto' }}
+        level={3}
+      >
         <MapMarker
           position={{ lat: 37.547450833697894, lng: 127.20462375027948 }}
           clickable={false}
