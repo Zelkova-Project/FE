0813a998be2fa7034ebeab307620bff8fbaa1286
tomhoon <gnsdl9108@naver.com>
2024-10-09 23:11:57 +0900

    fix: Minified React error 해결

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -126,7 +126,7 @@ const Nav = () => {
         ) : (
           <li className={'profile'} onClick={() => navigate('/profile')}>
             <label>
-              {userInfo.nickname}
+              {/* {userInfo.nickname}/ */}
               <div className={'profile-img'}>
                 <img src={'/default-profile-img.png'} alt={'프로필 사진'} />
               </div>
