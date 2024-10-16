8df15acaaf69e4c241dcd3178a68e5bd2a20c775
TomHoon <99778185+TomHoon@users.noreply.github.com>
2024-10-16 16:15:10 +0900

    Update axiosInstance.js

--- src/axios/axiosInstance.js
+++ src/axios/axiosInstance.js
@@ -4,15 +4,15 @@ const isDev = process.env.NODE_ENV == 'development';
 
 const instance = axios.create({
   baseURL: isDev ? 'http://localhost:8080/api' : '/api',
-  timeout: 1000,
+  timeout: 30000,
 });
 
-const getToken = () => {
-  let cookie = document.cookie;
-  const [key, val] = cookie.split('=');
-
-  return val;
-};
+function getCookie(name) {
+  let matches = document.cookie.match(new RegExp(
+    "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+  ));
+  return matches ? decodeURIComponent(matches[1]) : undefined;
+}
 
 instance.interceptors.request.use(
   (config) => {
@@ -27,7 +27,7 @@ instance.interceptors.request.use(
     // }
 
     // to-be
-    config.headers['Authorization'] = 'Bearer ' + getToken();
+    config.headers['Authorization'] = 'Bearer ' + getCookie('accessToken');
 
     return config;
   },
@@ -47,7 +47,7 @@ instance.interceptors.response.use(
       status: response.status,
       error: false,
       message: response.statusText,
-      data: response.data,
+      data: response?.data ? response.data : data,
     };
   },
   async (error) => {
@@ -65,10 +65,16 @@ instance.interceptors.response.use(
     //   return response;
     // }
     // throw new Error('error가 떴음 ', error);
-    const { response } = error;
-    console.error('error ', error);
-    
-    return { status: 404, error: error, message: response.data };
+    try {
+      const { response } = error;
+      console.error('error ', error);
+      
+      return { status: 404, error: error, message: response.data };
+    } catch(e) {
+      
+      console.error('error ', e);
+      return { status: 404, error: error, message: e };
+    }
   },
 );
 
