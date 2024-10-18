5d74c5d981b00513754de357f0a9d93764815f80
TomHoon <99778185+TomHoon@users.noreply.github.com>
2024-10-18 15:59:47 +0900

    feat: fileUtil 추가

---
+++ src/utils/fileUtil.js
@@ -0,0 +1,56 @@
+// 이미지 리사이즈
+
+// png, jpeg -> webp (용량 축소를 위해)
+
+
+const transferWebp = async (file) => {
+    let blobData = '';
+
+    const reader = new FileReader();
+    const 원본파일이름 = file.name;
+
+    let canvas = document.createElement("canvas");
+
+    await new Promise((res, rej) => {
+        reader.onload = (e) => {
+            let img = new Image();
+            img.src = e.target.result;
+
+            img.onload = () => {
+                canvas.width = img.width / 3.5;
+                canvas.height = img.height / 3.5;
+                const ctx = canvas.getContext('2d');
+
+                ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+
+                canvas.toBlob((blob) => {
+                    blobData = blob;
+                    console.log('inside >> ');
+                    res();
+                }, "image/webp", 0.8);
+            }
+        }
+        reader.readAsDataURL(file);
+    });
+
+    console.log('outside blobData >>> ', blobData);
+    return blobData;
+    // promise.then((res) => {
+    //     blobData = res;
+    //     console.log('blobData >>> ', blobData);
+    //     return blobData;
+    // })
+}
+
+const sendWebp = async (blobData, 원본파일이름) => {
+    const formData = new FormData();
+    formData.append("file", blobData, 원본파일이름);
+
+    const { error, status, data } = await axios.post("/files/upload", param);
+    return 원본파일이름;    
+}
+
+export {
+    transferWebp,
+    sendWebp
+}
