ad79459baaff1bd1374ae9b0ae5a28185833f78c
TomHoon <99778185+TomHoon@users.noreply.github.com>
2024-10-18 15:58:55 +0900

    fix: 로그인 관련 util로 따로 관리

--- src/pages/members/KakaoRedirectPage.jsx
+++ src/pages/members/KakaoRedirectPage.jsx
@@ -8,6 +8,8 @@ import { useNavigate } from 'react-router-dom';
 import { useRecoilState } from 'recoil';
 import { loginState, userInfoState } from '../../recoilState/recoil';
 
+import { deleteAllCookies, putCookie } from '../../utils/loginUtil';
+
 const KakaoRedirectPage = () => {
   const [login, setLogin] = useRecoilState(loginState);
   const [userInfo, setUserInfo] = useRecoilState(userInfoState);
@@ -17,24 +19,13 @@ const KakaoRedirectPage = () => {
   const [searchParam] = useSearchParams();
   const authCode = searchParam.get('code');
 
-  const deleteAllCookies = () => {
-    const cookies = document.cookie.split(";"); // Get all cookies
-  
-    for (let i = 0; i < cookies.length; i++) {
-      const cookie = cookies[i];
-      const eqPos = cookie.indexOf("=");
-      const name = eqPos > -1 ? cookie.slice(0, eqPos) : cookie;
-  
-      document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT;path=/";
-    }
-  }
-
   useEffect(() => {
     getAccessToken(authCode).then((access_token) => {
       getMemberWithAccessToken(access_token).then((res) => {
         deleteAllCookies();
+
         // 쿠키에 토큰 넣기
-        document.cookie = "accessToken=" + res.data.accessToken + '; max-age=604800; path=/';
+        putCookie(res.data.accessToken);
 
         // 임시로 모두 main으로 처리
         if (res.isSocial) {
